673d4733af79d7777a2d893dcf41b09332b7c3ae
Ao Zeng <zengaonc@gmail.com>
2018-04-05 18:12:09 -0400

    might need further tests

--- exp.py
+++ exp.py
@@ -14,6 +14,9 @@ class Quality:
     quality = {}
 
 class BandWidth:
+    '''
+    BandWidth - A wrapper of array, where the ith element indicates the bandwidth at i*unitTime
+    '''
     unitTime = 1.0
     def __init__(self, unit, bwList):
         '''
@@ -30,11 +33,6 @@ class Chunk:
     '''
     Chunk -- represents a chunk of video with fixed length
     '''
-    counter = 0 # indicate which round the chunk is added to the stream.streamBuffer
-    size = 0 # the number of bytes of the content
-    layer = 0 # which layer does this chunk belong to
-    timeLen = 0.0 # time length for each content
-    sentTime = 0.0 # This is set when the packet is sent to the server
 
     def __init__(self, newSize, newLayer, newQuality, newTimeLen):
         '''
@@ -45,13 +43,15 @@ class Chunk:
         metrics for quality measurement wrapped as a Quality object
         :param newTimeLen: number of second of video this chunk contains
         '''
-        self.size = newSize
-        self.layer = newLayer
+        self.counter = 0 # indicate which round the chunk is added to the stream.streamBuffer
+        self.size = newSize # the number of bytes of the content
+        self.layer = newLayer # which layer does this chunk belong to
         self.quality = newQuality
-        self.timeLen = newTimeLen
+        self.timeLen = newTimeLen # time length for each content
 
     def setSent(self, sentTime):
-        self.sentTime = sentTime
+        # Set the time stamp when it is sent
+        self.sentTime = sentTime # This is set when the packet is sent to the server
 
     def setCounter(self, counter):
         self.counter = counter
@@ -61,10 +61,10 @@ class Buffer:
     Buffer -- represents the uploader's in memory buffer,
     which holds encoded video chunks
     '''
-    layerNum = 3
-    buffer = []
 
     def __init__(self, numLayer = 3):
+        self.layerNum = numLayer
+        self.buffer = []
         for i in range(numLayer):
             self.buffer.append(deque())
         self.layerNum = numLayer
@@ -81,25 +81,30 @@ class Buffer:
 
         :param streamChunks - the buffer contains all the chunks that will be produced by video encoder
         :param timeElapse - number of seconds passed since the last addStreamChunks happened
-        :return:
+        :return: True if the StreamChunks is empty Flase otherwise
         '''
+        totalSliceNum = min(totalSliceNum, len(streamChunks[0]))
+
         for layer in range(streamChunks.layerNum):
             for sliceNum in range(totalSliceNum):
                 newChunk = streamChunks[layer].popleft()
-                # try:
-                #     newChunk = streamChunks[layer].popleft()
-                # except:
-                #     type, value, tb = sys.exc_info()
-                #     traceback.print_exc()
-                #     pdb.post_mortem(tb)
                 newChunk.setCounter(chunkCount + sliceNum + 1)
                 self.buffer[layer].append(newChunk)
 
+        return streamChunks.empty()
+
     def empty(self):
+        # Check if the whole buffer is totally empty
         result = True
         for layer in range(self.layerNum):
             result = result and len(self.buffer[layer]) == 0
         return result
+
+    def remainChunks(self):
+        chunkNum = 0
+        for i in range(self.layerNum):
+            chunkNum += len(self.buffer[i])
+        return chunkNum
 
 
 class Stream:
@@ -148,17 +153,29 @@ class Stream:
     def run(self):
         # the starting period, where self.latency seconds of video will be transmitted to the server
         self.streamBuffer.addStreamChunks(self.streamChunks, self.latencyWinSize, self.chunkCount)
-        #pdb.set_trace()
+        # Debug Purpose
+        errorhit = 0
+        prevTime = self.timeCounter
 
         while not self.streamChunks.empty() or not self.streamBuffer.empty():
             if not self.streamBuffer.empty():
                 currChunk = self.getNextChunk()
                 self.send(currChunk)
+                sliceNum = int((self.timeCounter - prevTime) / self.chunkLen)
+                if sliceNum > 0:
+                    assert(sliceNum == 1)
+                    self.streamBuffer.addStreamChunks(self.streamChunks, sliceNum, self.chunkCount)
+                    self.chunkCount += sliceNum
+                    prevTime = self.timeCounter
+
+
             else:
-                # @TODO: need to increment timer and add new chunks into stream buffer
+                errorhit += 1
+                # increment timer forward and add new chunks into stream buffer
                 self.timeCounter = (math.floor(self.timeCounter/self.chunkLen)+1)*self.chunkLen
                 self.streamBuffer.addStreamChunks(self.streamChunks, 1, self.chunkCount)
                 self.chunkCount += 1
+                prevTime = self.timeCounter
 
 
     def send(self, chunk):
@@ -168,7 +185,6 @@ class Stream:
         :param chunk - the chosen chunk to be sent to the server
         :return:
         '''
-        startTime = self.timeCounter
         currBWIdx = int(self.timeCounter/self.bwList.unitTime)
         timeLeft = (currBWIdx+1) * self.bwList.unitTime - self.timeCounter
         currBW = self.bwList[currBWIdx]
@@ -178,7 +194,6 @@ class Stream:
                 size -= currBW * timeLeft
                 self.timeCounter += timeLeft
                 currBWIdx += 1
-                print currBWIdx
                 currBW = self.bwList[currBWIdx]
                 timeLeft = self.bwList.unitTime
             else:
@@ -187,12 +202,6 @@ class Stream:
                 self.outputBuffer.addChunk(chunk)
                 # Add new chunk into streamBuffer
                 # if stream chuncks is not empty
-                timeElapsed = self.timeCounter - startTime
-                sliceNum = int(timeElapsed/self.chunkLen)
-                if len(self.streamChunks[0]) > 0:
-                    sliceNum = min(sliceNum, len(streamChunks[0]))
-                    self.streamBuffer.addStreamChunks(self.streamChunks,sliceNum, self.chunkCount)
-                    self.chunkCount += sliceNum
                 return
 
     def getNextChunk(self):
@@ -211,17 +220,22 @@ class Stream:
         for layer in range(self.numLayer):
             heads.append(self.getHead(layer))
             tails.append(self.getTail(layer))
-        minPair = heads[0]
+        maxPair = heads[0]
 
         for pair in heads:
-            if pair[2] > minPair:
-                minPair = pair
+            if pair[2] > maxPair[2]:
+                maxPair = pair
         for pair in tails:
-            if pair[2] > minPair:
-                minPair = pair
-        resultChunk = self.streamBuffer[minPair[0]][minPair[1]]
-        del self.streamBuffer[minPair[0]][minPair[1]]
+            if pair[2] > maxPair:
+                maxPair = pair
+        try:
+            resultChunk = self.streamBuffer[maxPair[0]][maxPair[1]]
+        except:
+            type, value, tb = sys.exc_info()
+            traceback.print_exc()
+            pdb.post_mortem(tb)
 
+        del self.streamBuffer[maxPair[0]][maxPair[1]]
         assert resultChunk is not None # Sanity Check
 
         return resultChunk
@@ -229,14 +243,20 @@ class Stream:
 
     def getHead(self, layerNum):
         currLayer = self.streamBuffer[layerNum]
+        if len(currLayer) == 0:
+            return (layerNum, 0, -1)
         idx = len(currLayer) - 1
-        while idx >= 0 and currLayer[idx] > self.chunkCount - self.latencyWinSize + 1:
+        while idx > 0 and currLayer[idx] > self.chunkCount - self.latencyWinSize + 1:
             idx -= 1
         val = self.headsCo[layerNum]
         return (layerNum, idx, val)
 
     def getTail(self,layerNum):
         currLayer = self.streamBuffer[layerNum]
+        if len(currLayer) == 0:
+            return (layerNum, 0, -1)
+            # @TODO: Here use negative num to indicate the current layer is empty, might need
+            # Might need better way to indicate this when the value can be negative
         val = (self.chunkCount - currLayer[0].counter) * self.tailsCo[layerNum]
         return (layerNum, 0, val)
 
@@ -251,6 +271,9 @@ class Stream:
         exist = False
         resultChunk = None
         currLayer = self.streamBuffer[layerNum]
+        if len(self.streamBuffer[layerNum]) == 0:
+            return False, None
+
         if self.chunkCount < self.latencyWinSize:
             resultChunk = currLayer.popleft()
             exist = True
@@ -279,20 +302,38 @@ if __name__ == "__main__":
     print "testing"
     streamChunks = Buffer(3)
     totalLayer = 3
-    layer0Size = 52427*1000
-    layer1Size = 53656*1000
-    layer2Size = 53663*1000
+    layer0Size = 100
+    layer1Size = 110
+    layer2Size = 120
     for i in range(100):
         streamChunks.addChunk(Chunk(layer0Size, 0, layer0Size/2, 2)) #layer 0
         streamChunks.addChunk(Chunk(layer1Size, 1, (layer0Size + layer1Size) / 2, 2)) # layer 1
         streamChunks.addChunk(Chunk(layer2Size, 2, (layer0Size + layer1Size + layer2Size)/ 2, 2)) # layer 2
 
-    bandWidths = BandWidth(1.0, [50000*1000*2] * 1000)
+    bandWidths = BandWidth(1.0, [100] * 1000)
 
-    #pdb.set_trace()
     stream = Stream(3, 6, streamChunks, bandWidths, None)
 
     stream.run()
+
     pdb.set_trace()
+    # print "testing"
+    # streamChunks = Buffer(3)
+    # totalLayer = 3
+    # layer0Size = 52427*1000
+    # layer1Size = 53656*1000
+    # layer2Size = 53663*1000
+    # for i in range(100):
+    #     streamChunks.addChunk(Chunk(layer0Size, 0, layer0Size/2, 2)) #layer 0
+    #     streamChunks.addChunk(Chunk(layer1Size, 1, (layer0Size + layer1Size) / 2, 2)) # layer 1
+    #     streamChunks.addChunk(Chunk(layer2Size, 2, (layer0Size + layer1Size + layer2Size)/ 2, 2)) # layer 2
+    #
+    # bandWidths = BandWidth(1.0, [50000*1000*2] * 1000)
+    #
+    # #pdb.set_trace()
+    # stream = Stream(3, 6, streamChunks, bandWidths, None)
+    #
+    # stream.run()
+    # pdb.set_trace()
 
 
